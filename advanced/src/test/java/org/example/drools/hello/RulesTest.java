d0fc34f5b159371d91e63be984c5495417919025
Sven Ehrke <sven.ehrke@finnova.com>
2016-02-03 11:31:37 +0100

    started advanced example

---
+++ advanced/src/test/java/org/example/drools/hello/RulesTest.java
@@ -0,0 +1,33 @@
+package org.example.drools.hello;
+
+import org.drools.RuleBase;
+import org.drools.WorkingMemory;
+import org.drools.compiler.DroolsParserException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class RulesTest {
+
+	Context context = new Context("/org/example/drools/hello/helloWorld.drl");
+
+	@Test
+	public void shouldFireHelloWorld() throws IOException, DroolsParserException {
+		RuleBase ruleBase = context.initialiseDrools();
+		WorkingMemory workingMemory = context.initializeMessageObjects(ruleBase, this::createHelloWorld);
+
+		int expectedNumberOfRulesFired = 1;
+		int actualNumberOfRulesFired = workingMemory.fireAllRules();
+		assertThat(actualNumberOfRulesFired, is(expectedNumberOfRulesFired));
+	}
+
+	private void createHelloWorld(WorkingMemory workingMemory) {
+		Message helloMessage = new Message();
+		helloMessage.setType("Hello");
+		workingMemory.insert(helloMessage);
+	}
+}
